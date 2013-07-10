311a9a733ce4a0c6da9bd6fb1e1b484e0e55be72
Murilo Andrade <murilove@gmail.com>
2013-07-10 00:01:05 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var str = fs.readFileSync('index.html','utf-8');
+
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
